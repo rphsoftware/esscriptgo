9a7bb84f224fd140bbd06854726302fb1261b7f4
Rph <rphsoftware@gmail.com>
2020-01-27 22:09:32 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 /*
 	ESScriptGO - A golang implementation of Extrasklep's scripting language (https://github.com/extrasklep/lang)
 	Copyright (C) 2019 Rph
-
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,12 +16,13 @@
 	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	SOFTWARE.
-	
-	Changelog: v1.0.2:
+
+	Changelog: v1.0.3:
 	Changes since 1.0:
-		-fix comments
+		- fix comments
 		- fix line numbering underflow error
- */
+		- removed logging of all commands
+*/
 
 
 package main
@@ -578,8 +576,10 @@ func main() {
 		parsedLines[index] = thisLine
 		parsedLines[index].lineNum = parsedLines[index].lineNum + 1
 	}
-
-	fmt.Println(parsedLines)
+	
+	if dbgL > 3 {
+		fmt.Println(parsedLines)
+	}
 
 	execute(parsedLines)
 }
